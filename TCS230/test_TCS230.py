b9fab68ef67a5a8510d06d286cd9d2d4b2650c84
Noel <gsm731@yandex.ru>
2016-02-26 17:37:50 +0200

    datchik cveta rabotaet v temnote

--- TCS230/test_TCS230.py
+++ TCS230/test_TCS230.py
@@ -17,12 +17,12 @@ GPIO.output(16, True)
 GPIO.output(18, False)
 
 
-def PulseIn(pin, val):
+def PulseIn(val):
 	beg_impulse = 0
 	end_impulse = 0
-	while GPIO.input(pin) == val:
+	while GPIO.input(12) == val:
 		beg_impulse = time.time()
-	while GPIO.input(pin) != val :
+	while GPIO.input(12) != val :
 		end_impulse = time.time()
 	dur_impulse = end_impulse - beg_impulse
 	return dur_impulse
@@ -30,25 +30,25 @@ def PulseIn(pin, val):
 def ReadRed():
 	GPIO.output(8, False)
 	GPIO.output(10, False)
-	RedColor = PulseIn(12, False)
+	RedColor = PulseIn(False)
 	return RedColor
 
 def ReadGreen():
 	GPIO.output(8, True)
 	GPIO.output(10, True)
-	GreenColor = PulseIn(12, False)
+	GreenColor = PulseIn(False)
 	return GreenColor
 
 def ReadBlue():
 	GPIO.output(8, False)
 	GPIO.output(10, True)
-	BlueColor = PulseIn(12, False)
+	BlueColor = PulseIn(False)
 	return BlueColor
 
 def ReadWhite():
 	GPIO.output(8, True)
 	GPIO.output(10, False)
-	WhiteColor = PulseIn(12, False)
+	WhiteColor = PulseIn(False)
 	return WhiteColor
 
 
@@ -57,16 +57,26 @@ r0 = ReadRed() /1455971442*255
 g0 = ReadGreen()/1455971442*255
 b0 = ReadBlue() /1455971442*255
 while True:  
-	w = (ReadWhite()/1455971442*255 - w0) * 1000000
-	r = (ReadRed()/1455971442*255 - r0)  * 1000000
+	w = (ReadWhite()/1455971442*255 - w0) * 10000000000
+	r = (ReadRed()/1455971442*255 - r0)  * 1000000000000
 	g = (ReadGreen()/1455971442*255 - g0) * 1000000000000
 	b = (ReadBlue()/1455971442*255 - b0) * 1000000000000
+
+	if (w>=100) or (w<=-100):
+		w = 10
+	if (r>=100) or (r<=-100):
+		r = 10
+	if (g>=100) or (g<=-100):
+		g = 10
+	if (b>=100) or (b<=-100):
+		b = 10
+
 
 	print("white:", w)
 	print("red:  ", r)
 	print("green:", g)
 	print("blue: ", b)
-	time.sleep(0.4)
+	time.sleep(0.1)
 	
 	if (b > -35 ) and (b < -28 ) and (g > -39) and (g < -33):
 		print "red"
